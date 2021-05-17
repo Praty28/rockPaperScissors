0b2d5f9f1401d7fdc47a83ca927ff90d808102b3
Pratyush Mendon <mendonpratyush908@gmail.com>
2021-05-17 22:55:33 +0530

    Add a couple of variables and add function gameTime with if else statements for win/loss

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 let compMove = ["Rock", "Paper", "Scissors"];
+let saveMove;
+let humanMove;
+let humanWin = 0;
+let compWin = 0;
 
 function rng(min, max) {
     return Math.floor(Math.random()*(max - min +1) );
 }
 
-function computerPlay () {
-    return compMove[rng(0,2)];
+function computerPlay() {
+    saveMove = compMove[rng(0,2)];
 }
 
+function gameTime () {
+    computerPlay();
+    humanMove = prompt("Select your move!");
+    
+    console.log (`Comp played ${saveMove} and you played ${humanMove.slice(0,1).toUpperCase() + humanMove.slice(1).toLowerCase()}.`)
 
-console.log(computerPlay());
+    if (humanMove.toLowerCase() == saveMove.toLowerCase()) {
+        console.log('It is a draw')
+    }
+
+    else if (humanMove.toLowerCase() == "rock" && saveMove.toLowerCase() == "paper") {
+
+        console.log('You Lose Human');
+    }
+    
+    else {
+        console.log('It\'s a wip');
+    }
+    console.log(compWin);
+}
+
+gameTime();
+
+
