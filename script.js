e69cae3ae0405d486d51b83e2ccd9daffa68ee35
Pratyush Mendon <mendonpratyush908@gmail.com>
2021-05-17 19:34:01 +0530

    Add function to create a random number and use that function to draw a value from an array at random

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+let compMove = ["Rock", "Paper", "Scissors"];
+
+function rng(min, max) {
+    return Math.floor(Math.random()*(max - min +1) );
+}
+
+function computerPlay () {
+    return compMove[rng(0,2)];
+}
 
 
+console.log(computerPlay());
